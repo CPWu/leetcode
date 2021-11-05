fac82c6d6f23685eb1d58ad28eac40a90e4b2f1c
Chun Wu <the.chun.wu@gmail.com>
2021-11-04 23:59:00 -0400

    Python - Leetcode #107 - Binary Tree Level Order Traversal II

--- Python/107_Binary_Tree_Level_Order_Traversal_II.py
+++ Python/107_Binary_Tree_Level_Order_Traversal_II.py
@@ -4,22 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+from collections import deque
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
             return []
-        
-        result, level = [], [root]
-        
-        while level:
-            currentNodes = []
-            nextLevel = []
-            for node in level:
-                currentNodes.append(node.val)
-                if node.left:
-                    nextLevel.append(node.left)
-                if node.right:
-                    nextLevel.append(node.right)
-            result.append(currentNodes)
-            level = nextLevel
-        return result
+    
+        queue = deque([root])
+        result = []
+
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                current_node = queue.popleft()
+                level.append(current_node.val)
+                
+                if current_node.left:
+                    queue.append(current_node.left)
+                if current_node.right:
+                    queue.append(current_node.right)
+            result.append(level)
+                
+        return result[::-1]
