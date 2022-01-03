94cb2829064b0e9634cf3f0923e5a4d08aa6b122
Chun Wu <the.chun.wu@gmail.com>
2022-01-03 00:03:21 -0500

    Python - Leetcode #415 - Add Strings

---
+++ Python/415_Add_Strings.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        i = len(num1) - 1
+        j = len(num2) - 1
+
+        carry = 0
+        result = ""
+
+        while i >=0 or j >= 0 or carry > 0:
+            if i >= 0:
+                carry += ord(num1[i]) - ord('0')
+                i -= 1
+            if j >= 0:
+                carry += ord(num2[j]) - ord('0')
+                j -= 1
+            result += str(carry%10)
+            carry = carry // 10
+
+        return result[::-1]
+            
+        
+soln = Solution()
+s1 = "2859"
+s2 = "293"
+print(soln.addStrings(s1, s2))
