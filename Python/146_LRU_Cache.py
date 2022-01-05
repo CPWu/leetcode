3cc4b99be646d61cdc367b5064c453ffb20535f4
Chun Wu <the.chun.wu@gmail.com>
2022-01-05 15:12:40 -0500

    Python - Leetcode #23 - Merge k Sorted Lists

--- Python/146_LRU_Cache.py
+++ Python/146_LRU_Cache.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict # So that we can use an ordered dictionary
 
 # Time: O(1), Space: O(capacity)
+# May also be done using Doubly Linked List
 class LRUCache(OrderedDict):
 
     def __init__(self, capacity):
@@ -8,17 +9,18 @@ class LRUCache(OrderedDict):
         :type capacity: int
         """
         self.capacity = capacity
+        self.cache = OrderedDict()
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
-        if key not in self:
+        if key not in self.cache:
             return -1
         
-        self.move_to_end(key)
-        return self[key]
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def put(self, key, value):
         """
@@ -26,11 +28,12 @@ class LRUCache(OrderedDict):
         :type value: int
         :rtype: None
         """
-        check = self.key_value.get(key, None)
-        if check:
-            self.key_value.move_to_end(key)
-        self.key_value[]
+        if key in self.cache:
+            del self.cache[key]
+        self.cache[key] = value
 
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
