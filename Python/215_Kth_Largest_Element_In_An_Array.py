38d1d180200188a41c5c15ed353e18bdce0ade6f
Chun Wu <the.chun.wu@gmail.com>
2022-02-14 16:15:11 -0500

    Python - Leetcode #215 - Kth Largest Element in an Array

--- Python/215_Kth_Largest_Element_In_An_Array.py
+++ Python/215_Kth_Largest_Element_In_An_Array.py
@@ -1,48 +1,39 @@
+# 3 Possible Solutions
+# 1. Sort Items
+# 2. Heap
+# 3. Quick Select
+
 class Solution(object):
-#     # Time: O(NlogN), Space: O(1)
-#     def findKthLargest(self, nums, k):
-#         """
-#         :type nums: List[int]
-#         :type k: int
-#         :rtype: int
-#         """
-#         nums.sort(reverse=True)
-        
-#         return nums[k-1]
+    # Time: O(NlogN), Space: O(1)
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return nums[len(nums)-k]
+
+    # Time: O(log k), Space: O(N log k)
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # nlargest method returns a list with n largets ements from the dataset.
+        # [-1] is required to return the last element in the list
+        return heapq.nlargest(k, nums)[-1]
 
     # Time: Avg(N)/Worst(N^2), Space: O(1)
     def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        def partition(left, right, pivot_index):
-            pivot = nums[pivot_index]
-            nums[pivot_index], nums[right] = nums[right], nums[pivot_index]
-            
-            store_index = left
-            for i in range(left, right):
-                if nums[i] < pivot:
-                    nums[store_index], nums[i] = nums[i], nums[store_index]
-                    store_index += 1
-            
-            nums[right], nums[store_index] = nums[store_index], nums[right]
-            
-            return store_index
+        k_index = len(nums) - k
         
-        def select(left, right, k_smallest):
-            if left == right:
-                return nums[left]
+        def quickSelect(leftPointer, rightPointer):
+            # Chosing last element in array as pivotElement
+            pivotElement = nums[rightPointer]
+            # Start pointer at left most value
+            pointer = leftPointer
             
-            pivot_index = random.randint(left, right)  
+            for i in range(leftPointer, rightPointer):
+                if nums[i] <= pivotElement:
+                    nums[pointer], nums[i] = nums[i], nums[pointer]
+                    pointer += 1
             
-            pivot_index = partition(left, right, pivot_index)
+            nums[pointer], nums[rightPointer] = nums[rightPointer], nums[pointer]
             
-            if k_smallest == pivot_index:
-                 return nums[k_smallest]
-            elif k_smallest < pivot_index:
-                return select(left, pivot_index - 1, k_smallest)
-            else:
-                return select(pivot_index + 1, right, k_smallest)
-        return select(0, len(nums) - 1, len(nums) - k)
+            if pointer > k_index: return quickSelect(leftPointer, pointer - 1)
+            elif pointer < k_index : return quickSelect(pointer + 1, rightPointer)
+            else: return nums[pointer]
+            
+        return quickSelect(0, len(nums) - 1)
