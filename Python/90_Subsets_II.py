6cde1a1a57227fe6d0ed57f4b096ed8406dd548e
Chun Wu <the.chun.wu@gmail.com>
2022-02-20 22:07:00 -0500

    Python - Leetcode #90 - Subsets II

--- Python/90_Subsets_II.py
+++ Python/90_Subsets_II.py
@@ -2,5 +2,38 @@
 # 1. Backtracking
 
 class Solution:
+    # Time: O(N*2^N), Space: O(n log n)
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        # Solution result
+        result = []
+
+        # Sort Array
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                result.append(subset[:])
+                return
+
+            # All subsets that include nums[i]
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+
+            # All subsets that dont include nums[i]
+            while i + 1 < len(nums) and nums[i] == nums[i+1]:
+                i += 1
+            
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+
+        return result
+
+
+
         
+
+
+
+    
