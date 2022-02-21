2481f65d809e768a00acfc3115907b9d6cc2d01f
Chun Wu <the.chun.wu@gmail.com>
2022-02-20 21:58:25 -0500

    Python - Leetcode #90 - Subsets II

--- Python/77_Combinations.py
+++ Python/77_Combinations.py
@@ -8,4 +8,17 @@ class Solution:
 
         combination = []
 
-        
+        def backtrack(start, combination):
+            if len(combination) == k:
+                result.append(combination[:])
+                return
+            
+            # not inclusive therefore n + 1
+            for i in range(start, n + 1):
+                combination.append(i)
+                backtrack(i + 1, combination)
+                combination.pop()
+
+        backtrack(1, combination)
+
+        return result
---
+++ Python/90_Subsets_II.py
@@ -0,0 +1,6 @@
+# 1 Possible Solutions
+# 1. Backtracking
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
