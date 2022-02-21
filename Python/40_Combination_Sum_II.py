5b4b082595138d155ba7d5d07707f5ab5a3b2caa
Chun Wu <the.chun.wu@gmail.com>
2022-02-21 13:54:07 -0500

    Python - Leetcode #494 - Target Sum

--- Python/40_Combination_Sum_II.py
+++ Python/40_Combination_Sum_II.py
@@ -1,4 +1,28 @@
 class Solution:
-    # Time: O(2^t), Space: O(2^t)
+    # Time: O(2^t), Space: O(N)
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+
+        result = []
+
+        def backtrack(position , currentCombo, target):
+            if target == 0:
+                result.append(currentCombo[:])
+                return
+            
+            if target <= 0:
+                return
+
+            prev = -1
+            for i in range(position, len(candidates)):
+                if candidates[i] == prev:
+                    continue
         
+                currentCombo.append(candidates[i])
+                backtrack(i + 1, currentCombo, target - candidates[i])
+                currentCombo.pop() 
+                prev = candidates[i]
+        
+        backtrack(0, [], target)
+
+        return result
---
+++ Python/494_Target_Sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    # Time: O(N*t), Space: O(t)
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        cache = {}
+
+        def backtrack(i, total):
+            if i == len(nums):
+                return 1 if total == target else 0
+
+            if (i, total) in cache:
+                return cache[(i, total)]
+
+            cache[(i, total)] = (backtrack(i + 1, total + nums[i]) + 
+                                backtrack(i + 1, total - nums[i]))
+
+            return cache[(i, total)]
+
+        return backtrack(0, 0)
