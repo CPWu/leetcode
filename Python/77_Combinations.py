874728804e0ecd5e4f6e57c65277a1f402e44907
Chun Wu <the.chun.wu@gmail.com>
2022-02-20 21:38:49 -0500

    Python - Leetcode #77 - Combinations

---
+++ Python/77_Combinations.py
@@ -0,0 +1,11 @@
+# 1 Possible Solutions
+# 1. Backtracking
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        result = []
+
+        combination = []
+
+        
