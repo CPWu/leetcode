9980da8e6a720cfc661996f107eac2156b8db063
Chun Wu <the.chun.wu@gmail.com>
2022-02-21 19:03:30 -0500

    Python - Leetcode #98 - Univalued Binary Tree

--- Python/98_Validate_Binary_Search_Tree.py
+++ Python/98_Validate_Binary_Search_Tree.py
@@ -14,17 +14,17 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
         # Nested Function
-        def valid(node, leftBoundary, rightBoundary):
+        def valid(node, left, right):
             # If BST is empty it is still a valid BST
             if not node:
                 return True
 
-            if node.val < rightBoundary and node.val > leftBoundary:
+            if not (node.val < right and node.val > left):
                 return False
 
             # Left subtree has to be less than the parent node.val)
             # Right subtree has to be greater than the parent node.val)
-            return (valid(node.left, leftBoundary, node.val) + valid(node.rightBoundary, node.val, right))             
+            return (valid(node.left, left, node.val) and valid(node.right, node.val, right))             
 
         return valid(root, float("-inf"), float("inf"))
 
