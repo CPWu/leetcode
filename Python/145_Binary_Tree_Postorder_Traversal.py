351976b44d28204d88382c241565fd28e687ee15
Chun Wu <the.chun.wu@gmail.com>
2022-02-23 20:59:48 -0500

    Python - Leetcode #791 - Custom Sort String

--- Python/145_Binary_Tree_Postorder_Traversal.py
+++ Python/145_Binary_Tree_Postorder_Traversal.py
@@ -1,3 +1,8 @@
+# 4 Possible Solutions
+# 1. Iterative - One Stack
+# 2. Iterative
+# 3. Recursive
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,26 +10,44 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-#     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-#         result = []
-#         self._inorderTraversal(root, result)
-#         return result
-    
-#     def _inorderTraversal(self, root, result):
-#         if root:
-#             self._inorderTraversal(root.left, result)
-#             result.append(root.val)
-#             self._inorderTraversal(root.right, result)
-
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        stack, result = [], []
+    # Two Stack
+    # Time: O(N), Space: O(H)
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # Empty Tree
+        if not root:
+            return None
         
-        while root or stack:
-            if root:
-                stack.append(root)
-                root = root.left
-            else:
-                node = stack.pop()
+        stack = [root]
+        result = []
+        
+        while stack:
+            currentNode = stack.pop()
+            result.append(currentNode.val)
+            
+            if currentNode.left:
+                stack.append(currentNode.left)
+                
+            if currentNode.right:
+                stack.append(currentNode.right)
+                
+        
+        return result[::-1]
+
+    # Recursion
+    # Time: O(N), Space: O(H)
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # Empty Tree
+        if not root:
+            return None
+        
+        result = []
+        
+        def _postOrderTraversal(node, result):
+            if node:
+                _postOrderTraversal(node.left, result)
+                _postOrderTraversal(node.right, result)
                 result.append(node.val)
-                root = node.right
+                
+        _postOrderTraversal(root, result)
+        
         return result
--- Python/791_Custom_Sort_String.py
+++ Python/791_Custom_Sort_String.py
@@ -1,11 +1,37 @@
-class Solution:
-    # Time: O(N), Space: O(N)
+# 2 Possible Solutions
+# 1. Dictionary to Index Order
+# 2. Counting Sort/Dictionary
+
+    # Time: O(NLogN), Space: O(1)
     def customSortString(self, order: str, s: str) -> str:
         lookup = defaultdict(int)
         i = 0
 
+        # Create lookup based on int order of order string.
         for character in order:
             lookup[character] = i
             i += 1
 
-        return "".join(sorted(s,key=lambda x:lookup[x]))
+        # Sort String
+        return "".join(sorted(s,key=lambda x:lookup.get(x, 100))
+
+
+    # Time: O(N), Space: O(1)
+    def customSortString(self, order: str, s: str) -> str:
+        # Use collections.Count to count frequency of characters in S
+        count = collections.Counter(s)
+        result = []
+        
+        # Write all characters that occur in S, in the order of S
+        for character in order:
+            result.append(character * count[character])
+            # Set count[character] = 0 to denote that we do not need to 
+            # write 'character' to our answer anymore
+            count[character] = 0
+            
+        # Write all remaining characters that wasnt in order string but in the input string
+        # to the end of the result.
+        for character in count:
+            result.append(character * count[character])
+            
+        return "".join(result)
