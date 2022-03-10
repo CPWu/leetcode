3fc52ddd03463780029712ca37e000a0b7396629
Chun Wu <the.chun.wu@gmail.com>
2022-03-10 12:58:36 -0500

    no message

--- Python/314_Binary_Tree_Vertical_Order_Traversal.py
+++ Python/314_Binary_Tree_Vertical_Order_Traversal.py
@@ -1,34 +1,39 @@
+# 2 Possible Solutions
+# 1. BFS with Sort
+# 2. BFS with no sort.
+
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-from collections import defaultdict, deque
-class Solution(object):
-#     # Time: O(NlogN), Space: O(N)
-#     def verticalOrder(self, root):
-#         """
-#         :type root: TreeNode
-#         :rtype: List[List[int]]
-#         """
-
-#         columnTable = defaultdict(list)
-#         queue = deque([(root, 0)])
+class Solution:
+    # Time: O(NlogN), Space: O(N)
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return none
         
-#         while queue:
-#             node, column = queue.popleft()
+        # Dictionary used to keep track of column
+        # Order the nodes within the dictionary list so we are aware of tree level
+        columnTable = defaultdict(list)
+        
+        queue = deque([(root, 0)])
+        
+        # While queue not empty, traverse:
+        while queue:
+            node, column = queue.popleft()
             
-#             if node is not None:
-#                 columnTable[column].append(node.val)
+            if node:
+                columnTable[column].append(node.val)
                 
-#                 queue.append((node.left, column - 1))
-#                 queue.append((node.right, column + 1))
+                queue.append((node.left, column - 1))
+                queue.append((node.right, column + 1))
                 
-#         return [columnTable[x] for x in sorted(columnTable.keys())]
-
+        return [columnTable[x] for x in sorted(columnTable.keys())]
+    
     # Time: O(N), Space: O(N)
+    # Keep track of min, max column so that we do not need to sort and achieve O(N) rather than NLogN
     def verticalOrder(self, root):
         """
         :type root: TreeNode
