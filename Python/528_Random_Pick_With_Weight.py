f143b756588c862e79b2faf533cc5921c4664ad5
Chun Wu <the.chun.wu@gmail.com>
2022-03-10 12:45:35 -0500

    Python - Leetcode #528 - Random Pick With Weight

--- Python/528_Random_Pick_With_Weight.py
+++ Python/528_Random_Pick_With_Weight.py
@@ -1,33 +1,41 @@
 import random
 
-class Solution(object):
-
-    def __init__(self, w):
-        """
-        :type w: List[int]
-        """
-        self.w = []
+class Solution:
+    # Time: O(N), Space: O(N)
+    def __init__(self, w: List[int]):
+        # List to contain our cumulative sums
+        self.cumulative_sum = []
         total = 0
+        # Function to calculate cumulative sum
         for weight in w:
             total += weight
-            self.w.append(total)
-        self.total = total
-
-    def pickIndex(self):
-        """
-        :rtype: int
-        """
+            self.cumulative_sum.append(total)
+        # Store the total
+        self.total = total 
         
-        # run a binary search to find the target zone
-        low, high = 0, len(self.w)
-        while low < high:
-            mid = low + (high - low) // 2
-            if target > self.w[mid]:
-                low = mid + 1
-            else:
-                high = mid
-        return low
+    # # Time: O(N), Space: O(1)   
+    # def pickIndex(self) -> int:
+    #     pickedNumber = random.randint(1, self.total)
+    #     for index, weight in enumerate(self.cumulative_sum):
+    #         if pickedNumber <= weight:
+    #             return index
     
+    # Time: O(log N), Space: O(1)
+    def pickIndex(self) -> int:
+        
+        pickedNumber = self.total * random.random()
+        
+        # Run a Binary Search
+        low, high = 0, len(self.cumulative_sum)
+        while low < high:
+            middle = low + (high - low) // 2
+            if pickedNumber > self.cumulative_sum[middle]:
+                low = middle + 1
+            else:
+                high = middle
+        return low
+
+
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
